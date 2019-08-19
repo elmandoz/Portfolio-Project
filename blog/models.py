08f9bb6fcd77915b7cf815d3ce65ba295ec41064
Mandoz <mohammadrizig@gmail.com>
2019-08-19 12:23:30 +0200

    Added bootstrap and all jobs are showing

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+import datetime
+from django.db.models.fields import (
+    DateField, DateTimeField, DurationField, Field, IntegerField, TimeField,
+)
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(blank=True, max_length=100)
+    pub_date = models.DateField(default=datetime.datetime.today)
+    body = models.TextField(blank=True)
+    image = models.ImageField(upload_to="images/")
