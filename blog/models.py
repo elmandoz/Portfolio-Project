532316c4f7c4e0bba3e3dc39606e90d6c822a2b0
Mandoz <mohammadrizig@gmail.com>
2019-08-20 16:41:19 +0200

    final project

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,21 @@ from django.db.models.fields import (
 )
 # Create your models here.
 class Blog(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(blank=True, max_length=100)
     pub_date = models.DateField(default=datetime.datetime.today)
     body = models.TextField(blank=True)
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
+
+
+
+
+    def __str__(self):
+        return '{} (ID:{})'.format(self.title, self.id)
+
+    def summary(self):
+        return self.body[:100]
+
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
